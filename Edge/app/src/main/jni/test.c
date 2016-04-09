660f0f8c1ffd5a432141f4686930e4c2e633c8fb
alexandrudsc <alexandru.dascalu94@gmail.com>
2016-04-09 17:34:39 +0300

    tested macros

--- Edge/app/src/main/jni/test.c
+++ Edge/app/src/main/jni/test.c
@@ -1,5 +1,6 @@
 #include <jni.h>
+#include "test.h"
 
 JNIEXPORT jint JNICALL Java_com_developer_alexandru_edge_MainActivity_add (JNIEnv * env, jobject obj, jint value1, jint value2) {
-    return value1 + value2;
+    return add(value1, value2);
 }
---
+++ Edge/app/src/main/jni/test.h
@@ -0,0 +1,10 @@
+//
+// Created by Alexandru on 4/9/2016.
+//
+
+#ifndef EDGE_TEST_H
+#define EDGE_TEST_H
+
+#define add(x, y) x + y
+
+#endif //EDGE_TEST_H
